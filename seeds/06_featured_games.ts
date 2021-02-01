4a5df610bbe95290fc23f18547e19ec2ead16c0d
Allan Legemaate <alegemaate@gmail.com>
2021-02-01 16:48:00 -0500

    Update s3 references

--- seeds/06_featured_games.ts
+++ seeds/06_featured_games.ts
@@ -5,7 +5,7 @@ export async function seed(knex: Knex): Promise<number[]> {
     .del()
     .then(() => {
       return knex("featured_game").insert([
-        { game_id: "059a1935-8950-4bb1-9acd-9e27e79741c4", place: 0 },
+        { game_id: "f81d51aa-610d-42ea-9cf1-187dd3832630", place: 0 },
         { game_id: "7ed5c0c0-396f-4222-80cc-30233e5edb93", place: 1 },
         { game_id: "da53d10c-3864-4189-bb87-4908ab5aad9b", place: 2 },
         { game_id: "5bd9d63e-fadd-4cae-918f-461396775cb5", place: 3 },
